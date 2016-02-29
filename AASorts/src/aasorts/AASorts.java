5dfc48e2c87a13261e8c41258efcc81b8a6ea398
Cesar <cesar777welch@gmail.com>
2016-02-28 22:33:40 -0600

    added radix

--- AASorts/src/aasorts/AASorts.java
+++ AASorts/src/aasorts/AASorts.java
@@ -35,7 +35,9 @@ public class AASorts {
                 //start
                 //quitar el comment del que quiera probar
                 //quickSort(arr,0,arr.length-1); 
-                heapSort(arr);
+                //heapSort(arr);
+                radixSort(arr);
+                
                 //selectionSort(arr);
                 //insertionSort(arr);
                 //mergeSort(arr);
@@ -51,6 +53,41 @@ public class AASorts {
 
     }
 
+      public static int[] radixSort(int[] old) {
+    // Loop for every bit in the integers
+    for (int shift = Integer.SIZE - 1; shift > -1; shift--) {
+        // The array to put the partially sorted array into
+        int[] tmp = new int[old.length];
+        // The number of 0s
+        int j = 0;
+ 
+        // Move the 0s to the new array, and the 1s to the old one
+        for (int i = 0; i < old.length; i++) {
+            // If there is a 1 in the bit we are testing, the number will be negative
+            boolean move = old[i] << shift >= 0;
+ 
+            // If this is the last bit, negative numbers are actually lower
+            if (shift == 0 ? !move : move) {
+                tmp[j] = old[i];
+                j++;
+            } else {
+                // It's a 1, so stick it in the old array for now
+                old[i - j] = old[i];
+            }
+        }
+ 
+        // Copy over the 1s from the old array
+        for (int i = j; i < tmp.length; i++) {
+            tmp[i] = old[i - j];
+        }
+ 
+        // And now the tmp array gets switched for another round of sorting
+        old = tmp;
+    }
+ 
+    return old;
+}
+        
     static int partition(int arr[], int left, int right)
 {
       int i = left, j = right;
